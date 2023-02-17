7f395307bc39a41cb2e692381241ad1779a306e1
robsoncardozo <robsonleaocardozo@hotmail.com>
2023-02-17 13:49:02 -0300

    workflow added

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ const grid = document.querySelector(".gridContainer");
 const userInput = document.getElementById("quantity");
 const resetButton = document.querySelector(".reset");
 
-const createGrid = (size) => {
-  for (let i = 0; i < size * size; i++) {
+createGrid = () => {
+  for (let i = 0; i < 256; i++) {
     const div = document.createElement("div");
     div.classList.add("square");
     grid.appendChild(div);
   }
 };
 
-const updateGrid = () => {
+updateGrid = () => {
   grid.innerHTML = "";
   grid.style.setProperty(
     "grid-template-columns",
@@ -20,37 +20,34 @@ const updateGrid = () => {
     "grid-template-rows",
     `repeat(${userInput.value}, 2fr)`
   );
-  createGrid(userInput.value);
-};
-
-const changeSquareColor = (e) => {
-  if (e.buttons === 1) {
-    e.target.style.backgroundColor = "black";
-  } else if (e.buttons === 2) {
-    e.target.style.backgroundColor = "red";
+  for (let i = 0; i < userInput.value * userInput.value; i++) {
+    const div = document.createElement("div");
+    div.classList.add("square");
+    grid.appendChild(div);
   }
 };
 
+const squares = document.querySelectorAll(".square");
+
+squares.forEach((square) => {
+  square.addEventListener("mousedown", function(event) {
+    if (event.button === 0) {
+      event.target.classList.add("color");
+    } else if (event.button === 2) {
+      event.preventDefault();
+      event.target.classList.add("red");
+    }
+  });
+});
+
 userInput.addEventListener("change", updateGrid);
 
-resetButton.addEventListener("click", function () {
+resetButton.addEventListener("click", function() {
   grid.innerHTML = "";
   userInput.value = "";
   grid.style.setProperty("grid-template-columns", `repeat(16, 2fr)`);
   grid.style.setProperty("grid-template-rows", `repeat(16, 2fr)`);
-  createGrid(16);
+  createGrid();
 });
 
-createGrid(16);
-
-grid.addEventListener("mousedown", (e) => {
-  e.preventDefault();
-  changeSquareColor(e);
-});
-
-grid.addEventListener("mouseover", (e) => {
-  e.preventDefault();
-  if (e.buttons === 1 || e.buttons === 2) {
-    changeSquareColor(e);
-  }
-});
+createGrid();
