c9602c39b58cb8770ee792503fd1c556c663a34f
robsoncardozo <robsonleaocardozo@hotmail.com>
2023-02-17 13:06:11 -0300

    update 2.0

--- script.js
+++ script.js
@@ -1,46 +1,47 @@
+// Selecionando elementos da DOM
 const grid = document.querySelector(".gridContainer");
 const userInput = document.getElementById("quantity");
 const resetButton = document.querySelector(".reset");
 
-createGrid = () => {
+// Cria a grade inicial
+function createGrid() {
   for (let i = 0; i < 256; i++) {
     const div = document.createElement("div");
     div.classList.add("square");
     grid.appendChild(div);
   }
-};
+}
 
-updateGrid = () => {
+// Atualiza a grade com o tamanho selecionado pelo usuário
+function updateGrid() {
   grid.innerHTML = "";
-  grid.style.setProperty(
-    "grid-template-columns",
-    `repeat(${userInput.value}, 2fr)`
-  );
-  grid.style.setProperty(
-    "grid-template-rows",
-    `repeat(${userInput.value}, 2fr)`
-  );
+  grid.style.setProperty("grid-template-columns", `repeat(${userInput.value}, 2fr)`);
+  grid.style.setProperty("grid-template-rows", `repeat(${userInput.value}, 2fr)`);
   for (let i = 0; i < userInput.value * userInput.value; i++) {
     const div = document.createElement("div");
     div.classList.add("square");
     grid.appendChild(div);
   }
-  console.log(userInput.value);
-};
+}
 
-const square = document.querySelector("div");
-square.addEventListener("mouseover", function(event) {
-  event.target.classList.replace("square", "color");
+// Altera a cor do quadrado ao passar o mouse sobre ele
+grid.addEventListener("mouseover", function(event) {
+  if (event.target.matches(".square")) {
+    event.target.classList.add("color");
+  }
 });
 
+// Atualiza a grade quando o usuário seleciona um novo tamanho
 userInput.addEventListener("change", updateGrid);
 
+// Reinicia a grade para o tamanho inicial quando o botão "RESET" é clicado
 resetButton.addEventListener("click", function() {
   grid.innerHTML = "";
   userInput.value = "";
-  grid.style.setProperty("grid-template-columns", `repeat(16, 2fr)`);
-  grid.style.setProperty("grid-template-rows", `repeat(16, 2fr)`);
+  grid.style.setProperty("grid-template-columns", "repeat(16, 2fr)");
+  grid.style.setProperty("grid-template-rows", "repeat(16, 2fr)");
   createGrid();
 });
 
+// Cria a grade inicial quando a página é carregada
 createGrid();
